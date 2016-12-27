0156c9bf7d4ec5116e9d361c536f50115aaef37a
guojuntao <guojuntao1@gmail.com>
2016-12-27 11:57:39 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## How to install guojuntao vimrc?
 
 	git clone https://github.com/guojuntao/vimrc.git ~/.vim_guojuntao
-	sh ~/.vim_runtime/install_guojuntao_vimrc.sh
+	sh ~/.vim_guojuntao/install_guojuntao_vimrc.sh
--- install_guojuntao_vimrc.sh
+++ install_guojuntao_vimrc.sh
@@ -5,11 +5,10 @@ echo 'set runtimepath+=~/.vim_guojuntao
 source ~/.vim_guojuntao/vimrc.vim
 ' > ~/.vimrc
 
-echo "Installed the GuoJuntao Vim configuration successfully! Enjoy :-)"
-
 # install vundle
 git submodule update --init
 
 # install vundle plugin
 vim +PluginInstall +qall
 
+echo "Installed the GuoJuntao Vim configuration successfully! Enjoy :-)"
