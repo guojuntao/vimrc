66b0ca01851e599340fee0ee212fe77e9d432bec
guojuntao <guojuntao@finogeeks.com>
2017-07-17 16:33:09 +0800

    modify taglist plugin

--- install_guojuntao_vimrc.sh
+++ install_guojuntao_vimrc.sh
@@ -5,6 +5,13 @@ echo 'set runtimepath+=~/.vim_guojuntao
 source ~/.vim_guojuntao/vimrc.vim
 ' > ~/.vimrc
 
+# 在 golang 中使用 taglist
+echo '--langdef=Go
+--langmap=Go:.go
+--regex-Go=/func([ \t]+\([^)]+\))?[ \t]+([a-zA-Z0-9_]+)/\2/f,func/
+--regex-Go=/var[ \t]+([a-zA-Z_][a-zA-Z0-9_]+)/\1/v,var/
+--regex-Go=/type[ \t]+([a-zA-Z_][a-zA-Z0-9_]+)/\1/t,type/' > ~/.ctags
+
 # install vundle
 # TODO: use subtree instead submodule
 git submodule update --init
