bc8e9e1c494e78817d7ef54560023ee4b26348e9
guojuntao <guojuntao1@gmail.com>
2017-07-23 15:49:50 +0800

    modify commit

--- install_guojuntao_vimrc.sh
+++ install_guojuntao_vimrc.sh
@@ -5,13 +5,6 @@ echo 'set runtimepath+=~/.vim_guojuntao
 source ~/.vim_guojuntao/vimrc.vim
 ' > ~/.vimrc
 
-# 在 golang 中使用 ctags 
-echo '--langdef=Go
---langmap=Go:.go
---regex-Go=/func([ \t]+\([^)]+\))?[ \t]+([a-zA-Z0-9_]+)/\2/f,func/
---regex-Go=/var[ \t]+([a-zA-Z_][a-zA-Z0-9_]+)/\1/v,var/
---regex-Go=/type[ \t]+([a-zA-Z_][a-zA-Z0-9_]+)/\1/t,type/' > ~/.ctags
-
 # install vundle
 # TODO: use subtree instead submodule
 git submodule update --init
